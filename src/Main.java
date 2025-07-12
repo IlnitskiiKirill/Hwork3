5dbc609a5c52152babd4cd4d9dc0e8e82282b6f1
hello word <ilnitskiykirill@gmail.com>
2025-07-12 12:14:32 +0300

    Исправление замечаний

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 
-            System.out.println("Задача №1 ");
+            System.out.println(" Задача №1 ");
 
             byte a = 13;
              System.out.println(" Значение переменной a c типом byte = " +a);
